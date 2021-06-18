595ea9c80fab622141d79f649bbfb4e7d24fcc7a
bestcarBJ <best_car@outlook.fr>
2021-06-19 00:23:51 +0100

    no check leaks

---
+++ new_pipe/ft_pipe.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipe.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbjaghou <nbjaghou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/10 17:23:56 by nbjaghou          #+#    #+#             */
+/*   Updated: 2021/06/15 18:47:42 by nbjaghou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	ft_pipe(void)
+{
+	int status;
+
+	pipe(g_data.fd);
+	g_data.pid = fork();
+	if (g_data.pid == -1)
+		exit(EXIT_FAILURE);
+	if (g_data.pid == 0)
+	{
+		dup2(g_data.fd[1], 1);
+		dup2(g_data.inp, 0);
+		close(g_data.fd[0]);
+		close(g_data.fd[1]);
+		execve(g_data.s1[0], g_data.s1, g_data.envp);
+	}
+	else
+	{
+		dup2(g_data.fd[0], 0);
+		dup2(g_data.out, 1);
+		close(g_data.fd[0]);
+		close(g_data.fd[1]);
+		execve(g_data.s2[0], g_data.s2, g_data.envp);
+	}
+	close(g_data.inp);
+	close(g_data.out);
+	waitpid(g_data.pid, &status, 0);
+	if (WIFEXITED(status))
+		g_ret = WEXITSTATUS(status);
+}
